c12e4fbe0e52735963f16020568204aa3bd90c3b
Sameer Ansari <sam3268212@gmail.com>
2025-01-23 18:47:00 +0530

    Completed city repository

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+
 const {PORT} = require('./config/serverConfig');
 const setupAndStartServer = async () => {
     
@@ -13,6 +14,7 @@ const setupAndStartServer = async () => {
 
     app.listen(PORT,() =>{
         console.log(`Server started at ${PORT}`);
+        const repo = new CityRepository();
     });
 }
 
