41a457dd8d01eab3adb42181369d1576500d9142
Sameer Ansari <sam3268212@gmail.com>
2025-02-08 16:25:32 +0530

    Added flights search crud with few filter

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -12,5 +12,7 @@ router.patch('/city/:id', CityController.update);
 
 
 router.post('/flights', FlightController.create);
+router.get('/flights', FlightController.getAll);
 
-module.exports = router;
+
+module.exports = router; 
